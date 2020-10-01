14451b0de7cea652e92fdefc116121535f75c91d
elitest <dcypherd@gmail.com>
2020-10-01 11:16:31 -0500

    Create jwtdecodenoverify.py

---
+++ jwt/jwtdecodenoverify.py
@@ -0,0 +1,9 @@
+# I think pip install pyjwt.
+# There are multiple libraries that go by 'jwt' as the module name, so you may want to run this in a venv.
+##TODO
+# Print off all the possible content types with a token according to the RFC and what they mean.
+# Take user input
+
+import jwt
+f = "#THE JWT Token#
+jwt.decode(f, verify=False)
